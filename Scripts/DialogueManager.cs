f0719f57de19a95f16c76bdfd1ff04b6b94cb9a7
Samuel Si <ssi.de@mysummitps.org>
2021-12-15 02:37:05 -0800

    updated the dialogue system

--- Scripts/DialogueManager.cs
+++ Scripts/DialogueManager.cs
@@ -3,51 +3,135 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class DialogueManager : MonoBehaviour
 {
 
     public Queue<string> sentences;
-    public TextMeshPro dialogueText;
+    public Queue<AudioClip> sentenceAudio;
+    public TMP_Text dialogueText;
     public float textspeed = 0.1f;
     public AudioSource audioSource;
+    public string currentSentence;
+    public bool runningText = false;
+
+    public DialogueScriptableObject loadedDialogue; //needs to be changed whenever you want to swap dialogues
+    public TMP_Text speakerText;
+    public GameObject dialogueParent;
+    public Image speakerImage;
+    public AudioClip currentAudio;
+    public int sentenceCount = 0;
+    public float startingYPos;
+    public GameObject targetPosObj;
+    public DialogueScriptableObject[] conversationStarters;
 
     void Start()
     {
+        startingYPos = dialogueParent.transform.position.y;
         audioSource = GetComponent<AudioSource>();
-        sentences = new Queue<string>();
+        sentences = new Queue<string>(); //first in last out?
+        
+        //StartCoroutine(LateStart(1));
     }
 
-    public void StartDialogue(Dialogue dialogue)
+    IEnumerator LateStart(float waitTime)
     {
-        Debug.Log("Starting conversation");
+        yield return new WaitForSeconds(waitTime);
+        StartDialogue();
+    }
+
+    public void SelectDialogue(string dialogue)
+    {
+        foreach (var convo in conversationStarters)
+        {
+
+            Debug.Log(convo.ToString());
+            if (convo.ToString() == dialogue + " (DialogueScriptableObject)")
+            {
+                loadedDialogue = convo;
+                StartDialogue();
+                break;
+            }
+        }
+    }
+    public void StartDialogue()
+    {
+        textspeed = loadedDialogue.speakerSpeed;
+        dialogueText.text = "";
+        speakerText.text = loadedDialogue.speaker;
+        speakerImage.sprite = loadedDialogue.speakerImage;
+        Tween tween = dialogueParent.transform.DOMove(targetPosObj.transform.position, .5f).SetEase(Ease.InOutQuad);
+        tween.OnComplete(StartDialogue2);
+
+    }
+    public void StartDialogue2()
+    { 
+
+        //Debug.Log("Starting conversation");
         sentences.Clear();
 
-        foreach (string sentence in dialogue.sentences)
+        foreach (string sentence in loadedDialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
 
         DisplayNextSentence();
+    }
+    public void StartNextDialogue()
+    {
+        loadedDialogue = loadedDialogue.nextDialogue;
+        textspeed = loadedDialogue.speakerSpeed;
+        speakerText.text = loadedDialogue.speaker;
+        speakerImage.sprite = loadedDialogue.speakerImage;
+        //Debug.Log("Starting conversation");
+        sentences.Clear();
 
+        foreach (string sentence in loadedDialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        
+
+        DisplayNextSentence();
     }
 
     public void DisplayNextSentence()
     {
+        audioSource.Stop();
+        runningText = true;
         if (sentences.Count == 0) //end of queue
         {
+
+            runningText = false;
             EndDialogue();
             return;
         }
-        string sentence = sentences.Dequeue();
+        if (loadedDialogue.sentenceAudio.Length > sentenceCount) //length 3, count 2
+        { 
+            currentAudio = loadedDialogue.sentenceAudio[sentenceCount];
+        }
+        else
+        {
+            currentAudio = null;
+        }
+        sentenceCount++;
+        currentSentence = sentences.Dequeue();
+
+        //Debug.Log(currentSentence);
         //dialogueText.text = sentence;
         StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence));
+        StartCoroutine(TypeSentence(currentSentence));
     }
 
     IEnumerator TypeSentence(string sentence)
     {
-        audioSource.Play();
+        if (currentAudio != null)
+        {
+
+            audioSource.clip = currentAudio;
+            audioSource.Play();
+        }
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray())
         {
@@ -55,12 +139,38 @@ public class DialogueManager : MonoBehaviour
             //yield return null;//waits a frame
             yield return new WaitForSeconds(textspeed);
         }
-        audioSource.Stop();
+        //audioSource.Stop();
+        runningText = false;
     }
 
+    public void FinishSentence()
+    {
+        StopAllCoroutines();
+        dialogueText.text = currentSentence;
+        if (runningText == false)
+        {
+            DisplayNextSentence();
+        }
+        else
+        { 
+            runningText = false;
+        }
+
+    }
     void EndDialogue()
     {
-        Debug.Log("End of conversation");
+        sentenceCount = 0;
+        if (loadedDialogue.nextDialogue == null)
+        {
+
+            //Debug.Log("End of conversation");
+            //dialogueParent.SetActive(false);
+            Tween tween = dialogueParent.transform.DOMove(new Vector3(dialogueParent.transform.position.x, startingYPos, dialogueParent.transform.position.z), .5f).SetEase(Ease.InOutQuad);
+        }
+        else
+        {
+            StartNextDialogue();
+        }
     }
 
 }
---
+++ Scripts/DialogueScriptableObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(fileName = "newDialogue", menuName = "Dialogue")]
+public class DialogueScriptableObject : ScriptableObject
+{
+    public string speaker;
+    public Sprite speakerImage;
+    public float speakerSpeed = 0.01f;
+    [TextArea(3, 10)]
+    public string[] sentences;
+    public AudioClip[] sentenceAudio;
+    public DialogueScriptableObject nextDialogue;
+}
